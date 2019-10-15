74219b8420a5ec45e5799306f8789f147937520c
Chong Shen Ng <ngchongshen@gmail.com>
2019-10-15 15:05:29 +0200

    Update shortscripts.py

--- shortscripts.py
+++ shortscripts.py
@@ -5,3 +5,11 @@ print("Give an integer: ")
 num = int(input())
 out = list(primefac.primefac(num))
 print("The factors are: ", out)
+
+#-- Increase dimensionality of array
+x[np.newaxis,:]
+# or
+x[:,np.newaxis]
+
+#-- Tile arrays (replicate matrices)
+np.tile(x[np.newaxis,:],(2,1))
